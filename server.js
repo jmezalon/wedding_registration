48ef092f5ff1e5ae9480d757a3ab26fe1fe9c9ca
Max Mezalon <jmezalon@gmail.com>
2023-12-19 20:44:48 -0500

    setup env varable and configs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { PORT } = require("./config");
+
 const { NotFoundError } = require("./utils/errors");
 
 const app = express();
@@ -23,8 +25,6 @@ app.use((err, req, res, next) => {
     error: { message, status },
   });
 });
-
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running http://localhost:${PORT}`);
