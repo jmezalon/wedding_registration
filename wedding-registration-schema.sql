d161320447bbfb88cec25b31f44822efcf07ee86
Max Mezalon <jmezalon@gmail.com>
2023-12-19 20:18:48 -0500

    added database creation and scripts.

---
+++ wedding-registration-schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id         SERIAL PRIMARY KEY,
+    password   TEXT NOT NULL,
+    email      TEXT NOT NULL UNIQUE CHECK(POSITION('@' IN EMAIL) > 1),
+    rsvp_staus BOOLEAN NOT NULL,
+    num_guests INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
